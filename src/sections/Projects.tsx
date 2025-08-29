98b25183f7f7c675aed4ab4eb765469a48e7096b
samir0607 <samir.gupta987@gmail.com>
2025-08-30 01:49:19 +0530

    chore: first look

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,7 @@ export default function Projects() {
 	return (
 		<div className="h-screen">
 			<div className="text-5xl font-bold">Projects</div>
+			<div>What I have build so far</div>
 		</div>
 	);
 }

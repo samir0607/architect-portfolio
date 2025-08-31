a6d084f62fe25661ac60fd43140e81aaaf2b5cb7
samir0607 <samir.gupta987@gmail.com>
2025-08-31 16:34:58 +0530

    fiix: blue

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -10,14 +10,14 @@ export default function Intro() {
 				<div className="text-6xl text-gray-300 font-bold">Anushka Thebe</div>
 				<div className="mt-3 text-2xl">
 					I'm 
-					<TextType className="ml-3 w-[160px] bg-red-800 border rounded-xl shadow-[0_0_15px_3px_rgba(255,115,0,0.7),0_0_40px_10px_rgba(255,123,128,0.4)]" text={[" an Architect", " a Vlogger", " an Artist"]}/> 
+					<TextType className="ml-3 w-[160px] border rounded-xl shadow-[0_0_15px_3px_rgba(255,115,0,0.7),0_0_40px_10px_rgba(255,123,128,0.4)]" text={[" an Architect", " a Vlogger", " an Artist"]}/> 
 				</div>
 				<div className="text-lg text-gray mt-6"> 
 					driven by the vision of designing spaces that harmonize aesthetics, functionality, and sustainability. My approach blends creative vision with technical precision to design environments that inspire, endure, and enhance everyday life. 
 				</div>
 			</div>
 			<div className="flex justify-center h-[400] md:h-[600]">
-				<img src="profile.jpeg" alt="Loading..." className="rounded-2xl"/>
+				<img src="front.png" alt="Loading..." className="rounded-2xl"/>
 			</div>
 		</div>
 	</>

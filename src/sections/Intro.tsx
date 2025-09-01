1039e359259d20edf50356bebe4f892690e366c6
samir0607 <samir.gupta987@gmail.com>
2025-09-01 20:31:08 +0530

    fix: intro titles

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -10,7 +10,7 @@ export default function Intro() {
 				<div className="text-6xl text-gray-300 font-bold">Anushka Thebe</div>
 				<div className="mt-3 text-2xl">
 					I'm 
-					<TextType className="ml-3 w-[160px] border rounded-xl shadow-[0_0_15px_3px_rgba(0,115,180,0.7),0_0_40px_10px_rgba(0,223,128,0.4)]" text={[" an Architect", " a Vlogger", " an Artist"]}/> 
+					<TextType className="ml-3 w-[240px] border rounded-xl shadow-[0_0_15px_3px_rgba(0,115,180,0.7),0_0_40px_10px_rgba(0,223,128,0.4)]" text={[" an Architect Student", " a Passionate Vlogger", " a Creative Artist"]}/> 
 				</div>
 				<div className="text-lg text-gray mt-6"> 
 					driven by the vision of designing spaces that harmonize aesthetics, functionality, and sustainability. My approach blends creative vision with technical precision to design environments that inspire, endure, and enhance everyday life. 
